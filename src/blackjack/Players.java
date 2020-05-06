9b94cd0910422dd6a2db342a68a97630f5f3f58f
MariaRamlochan <mariaramlochan@hotmail.com>
2020-05-05 23:19:30 -0400

    Class players

---
+++ src/blackjack/Players.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blackjack;
+
+/**
+ *
+ * @author maria
+ */
+public class Players {
+    private String name;
+    private int age;
+    
+    public Players(String name) {
+        this.name = name;
+    }
+    
+    public Players (String name, int age) {
+        this.age = age;
+    }
+    
+    //getters
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    
+    //setters
+    public void setName() {}
+    
+}
+
